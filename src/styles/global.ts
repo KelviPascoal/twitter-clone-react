ab8b8897cf32f5c744a883e5d4b5ebc473d93ced
kelvi <kelvi_ugo@hotmail.com>
2022-06-26 21:19:15 -0300

    feat: configura global styles

---
+++ src/styles/global.ts
@@ -0,0 +1,33 @@
+import { createGlobalStyle, css } from "styled-components";
+
+export default createGlobalStyle`
+    ${({ theme }) => css`
+
+  * {
+    font-size: 62.5%;
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    border: 0;
+    background: none;
+    font-family: ${theme.font.family}
+  }
+  
+  html, body {
+    max-height: 100vh;
+    max-width: 100vw;
+    width: 100%;
+    height: 100%;
+  }
+
+  button {
+    cursor: pointer;
+  }
+
+  html {
+    background: ${theme.colors.primary};
+  }
+`}
+`;
