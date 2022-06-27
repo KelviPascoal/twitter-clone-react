60f7a0c99bb85ef5deaa1ace6a4247322cfa13cf
kelvi <kelvi_ugo@hotmail.com>
2022-06-26 21:18:29 -0300

    feat: cofigura theme

---
+++ src/styles/theme.ts
@@ -0,0 +1,37 @@
+export const theme = {
+  colors: {
+    primary: "#000",
+    secondary: "#15181C",
+    search: "#202327",
+    white: "#D9D9D9",
+    gray: "#7A7A7A",
+    outline: "#2F3336",
+    retweet: "#00C06B",
+    like: "#E8265E",
+    twitter: "#33A1F2",
+    twitterDarkHover: "#011017",
+    twitterLightHover: "#2C8ED6",
+  },
+  font: {
+    family:
+      "-apple-system, system-ui, BlinkMacSystemFont, 'Segoe UI', Roboto, 'Helvetica Neue', Ubuntu, Arial, sans-serif",
+    light: 300,
+    normal: 400,
+    bold: 600,
+    sizes: {
+      xsmall: "1.2rem",
+      small: "1.3rem",
+      medium: "1.5rem",
+      large: "2.0rem",
+      xlarge: "2.0rem",
+      xxlarge: "2.8rem",
+    },
+  },
+  breakpoints: {
+    xs: "320px",
+    sm: "500px",
+    md: "768px",
+    lg: "1440px",
+    // xl: "80em",
+  },
+};
